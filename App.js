46bc53d4aa72229714d8c497e91c650cbd7cde3c
Peter karanja <peterkaranja60@gmail.com>
2022-03-04 14:35:24 +0300

    Initial Update
    beginner app React-native

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>
+          Today's tasks
+        </Text>
+        <View style={styles.items}>
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+          <Task text={'Task 3'} />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +24,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginBottom: 20,
+    marginTop: 30,
   },
 });
